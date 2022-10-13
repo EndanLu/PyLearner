3a38676e343f11e44282e5c230107c55496e005b
EndanLu <luendan@yeah.net>
2022-10-13 16:28:08 +0800

    commit from PyCharm IDE.

---
+++ Basic.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+
+def testBasic():
+    a, b, c, d = 20, 5.5, True, 4+3j;
+
+    print(a, type(a))
+    print(b, type(b))
+    print(c, type(c))
+    print(d, type(d))
+
+    if type(c) == bool:
+        print('Statement is True')
+    else:
+        print('Statement is False')
+
+
+    ListAll: list[str] = ['item 1', 'item 2', 'item 3']   # Add type hint for total.
+    ListTiny = ['item tiny 1', 'item tiny 2']
+    ListAll += ListTiny
+    print(ListAll)
+
+    ListAll[2] = ''     # Change list item.
+    print(ListAll)
+
+    print('Daniel \nLu')
+    print(r'Daniel \nLu')   # r means the original string.
--- P3.py
+++ P3.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
 from typing import List
 import sys
+from FuncCollection import reverseWords
+from Basic import testBasic
 
-a, b, c, d = 20, 5.5, True, 4+3j;
+if __name__ == "__main__":
+    print('命令行参数为:')
+    for i in sys.argv:
+        print(i)
 
-print(a, type(a))
-print(b, type(b))
-print(c, type(c))
-print(d, type(d))
+    input = 'I like python3'
+    rw = reverseWords(input)
+    print(rw)
 
-if type(c) == bool:
-    print('Statement is True')
-else:
-    print('Statement is False')
-
-
-ListAll: list[str] = ['item 1', 'item 2', 'item 3']   # Add type hint for total.
-ListTiny = ['item tiny 1', 'item tiny 2']
-ListAll += ListTiny
-print(ListAll)
-
-ListAll[2] = ''     # Change list item.
-print(ListAll)
-
-print('Daniel \nLu')
-print(r'Daniel \nLu')   # r means the original string.
-
-print('================Python import mode==========================')
-print('命令行参数为:')
-for i in sys.argv:
-    print (i)
-
+    testBasic()
