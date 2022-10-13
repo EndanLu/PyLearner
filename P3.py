7ef2c74ad9eb2b1e8658f2eba98dd9c278ca2f01
EndanLu <luendan@yeah.net>
2022-10-13 15:33:52 +0800

    commit from PyCharm IDE.

--- P3.py
+++ P3.py
@@ -15,8 +15,10 @@ else:
     print('Statement is False')
 
 
-total: list[str] = ['item 1', 'item 2', 'item 3']   # Add type hint for total.
-print(total)
+ListAll: list[str] = ['item 1', 'item 2', 'item 3']   # Add type hint for total.
+ListTiny = ['item tiny 1', 'item tiny 2']
+ListAll += ListTiny
+print(ListAll)
 
 print('Daniel \nLu')
 print(r'Daniel \nLu')   # r means the original string.
