42c4e82ca564ebd388b612dffbfe8f9608989381
EndanLu <luendan@yeah.net>
2022-10-14 16:09:17 +0800

    commit from PyCharm IDE.

--- Basic.py
+++ Basic.py
@@ -20,6 +20,26 @@ def testBasic():
     print(r'Daniel \nLu')  # r means the original string.
 
 
+def testTypeConversion():
+    x = int('3')  # str -> int
+    y = float('4.2')  # str -> float
+    z = str(3.0)  # float -> str
+
+    num_int = 123
+    num_str = "456"
+
+    print("num_int 数据类型为:", type(num_int))
+    print("类型转换前，num_str 数据类型为:", type(num_str))
+
+    num_str = int(num_str)  # 强制转换为整型
+    print("类型转换后，num_str 数据类型为:", type(num_str))
+
+    num_sum = num_int + num_str
+
+    print("num_int 与 num_str 相加结果为:", num_sum)
+    print("sum 数据类型为:", type(num_sum))
+
+
 def testList():
     listAll: list[str] = ['item 1', 'item 2', 'item 3']  # Add type hint for total.
     listTiny = ['item tiny 1', 'item tiny 2'];  # Change list item
@@ -51,17 +71,17 @@ def testSet():
     a = set('abracadabra')
     b = set('alacazam')
 
-    print(a)    # Auto-remove duplicate item
-    print(b)    # Auto-remove duplicate item
+    print(a)  # Auto-remove duplicate item
+    print(b)  # Auto-remove duplicate item
     print(a - b)  # a 和 b 的差集
     print(a | b)  # a 和 b 的并集
     print(a & b)  # a 和 b 的交集
     print(a ^ b)  # a 和 b 中不同时存在的元素
 
+
 def testDictionary():
-    dictAll = {'name':'Daniel Lu', 'code':'681161', 'mail':'Daniel_E_Lu@vfc.com'}
+    dictAll = {'name': 'Daniel Lu', 'code': '681161', 'mail': 'Daniel_E_Lu@vfc.com'}
 
     print(dictAll)
     print(dictAll.keys())
     print(dictAll.values())
-
--- P3.py
+++ P3.py
@@ -16,4 +16,5 @@ if __name__ == "__main__":
     testList();
     testTuple();
     testSet();
-    testDictionary()
+    testDictionary();
+    testTypeConversion();
