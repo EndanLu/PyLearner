f2177f087b21aef429c5269e87b4a92c6c808ef4
EndanLu <luendan@yeah.net>
2023-04-23 20:12:08 +0800

    commit from PyCharm IDE.

--- FileOperation.py
+++ FileOperation.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 
-def testFileOperations():
+def testReadFile():
     #f = open("LinuxRef\\modify_commit.txt", "r")
     f = open("README.md", "r")
     print(f.read())
     f.close()
+    print("*****************************")
+
+    f = open("README.md", "r")
+    print(f.readline())
+    print(f.readline())
+    f.close()
+    print("*****************************")
+
+    f = open("README.md", "r")
+    for line in f:
+        print(line)
+    f.close()
+    print("*****************************")
--- P3.py
+++ P3.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     #testTryExcept();
     #testInput();
     testStringFormat();
-    testFileOperations();
+    testReadFile();
 
 
 
