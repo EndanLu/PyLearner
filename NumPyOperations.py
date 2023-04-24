f010f08fc2eff52690b39734ddfe2ca334be678c
EndanLu <luendan@yeah.net>
2023-04-24 17:06:21 +0800

    commit from PyCharm IDE.

--- NumPyOperations.py
+++ NumPyOperations.py
@@ -3,9 +3,33 @@
 import numpy as np
 
 def testNumPyBase():
+    print("NumPy Version: " + np.__version__)
+
     arr = np.array([1,2,3,4,5])
     print(arr)
     print(type(arr))
-    print("NumPy Version: " + np.__version__)
+
+    arr2 = np.array((1, 2, 3, 4, 5))
+    print(arr2)
+    print(type(arr2))
+
+    arr3 = np.array([[1, 2, 3], [4, 5, 6]])
+    print(arr3)
+
+    a = np.array(42)
+    b = np.array([1, 2, 3, 4, 5])
+    c = np.array([[1, 2, 3], [4, 5, 6]])
+    d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+    print("array dimension: " + str(a.ndim))
+    print("array dimension: " + str(b.ndim))
+    print("array dimension: " + str(c.ndim))
+    print("array dimension: " + str(d.ndim))
+
+def testNumPyElementPickUp():
+    arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+    print('2nd element on 1st dim: ', arr[0, 1])
+    print('5th element on 2nd dim: ', arr[1, 4])
+
 
 
--- P3.py
+++ P3.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     #testRemoveFile();
 
     testNumPyBase();
+    testNumPyElementPickUp();
 
 
 
