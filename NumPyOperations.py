06a5ebf9ed428391b1cfd2bea8e9d7055e752ed9
EndanLu <luendan@yeah.net>
2023-04-27 14:55:46 +0800

    commit from PyCharm IDE.

--- NumPyOperations.py
+++ NumPyOperations.py
@@ -87,4 +87,12 @@ def testNumPyCopyView():
 
 def testNumPyShape():
     arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-    print(arr.shape)
+    print("Shape of arr:", arr.shape)
+
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    newarr = arr.reshape(3, 4)
+    print(newarr)
+
+    newarr = arr.reshape(2, 3, 2)
+    print(newarr)
+    print(newarr.base)          # Reshape() return one view, not copy.
