8e82be24b62aa737d7bf608abbe05d38e537e8ec
EndanLu <luendan@yeah.net>
2023-05-08 14:15:17 +0800

    commit from PyCharm IDE.

--- NumPyOperations.py
+++ NumPyOperations.py
@@ -217,6 +217,22 @@ def testNumPySort():
 def testNumPyArrayFilter():
     arr = np.array([61, 62, 63, 64, 65])
     x = [True, False, True, False, True]
-
     newarr = arr[x]
+    print(newarr)
+
+    arr = np.array([61, 62, 63, 64, 65])
+    # 创建一个空列表
+    filter_arr = []
+
+    # 遍历 arr 中的每个元素
+    for element in arr:
+        # 如果元素大于 62，则将值设置为 True，否则为 False：
+        if element > 62:
+            filter_arr.append(True)
+        else:
+            filter_arr.append(False)
+
+    newarr = arr[filter_arr]
+
+    print(filter_arr)
     print(newarr)
