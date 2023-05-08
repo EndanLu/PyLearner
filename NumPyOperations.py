fae5df97edbf485e62ef747a6ebae2f1b8ff3d5f
EndanLu <luendan@yeah.net>
2023-05-08 14:18:44 +0800

    commit from PyCharm IDE.

--- NumPyOperations.py
+++ NumPyOperations.py
@@ -220,6 +220,7 @@ def testNumPyArrayFilter():
     newarr = arr[x]
     print(newarr)
 
+    # 找>62的数
     arr = np.array([61, 62, 63, 64, 65])
     # 创建一个空列表
     filter_arr = []
@@ -228,6 +229,24 @@ def testNumPyArrayFilter():
     for element in arr:
         # 如果元素大于 62，则将值设置为 True，否则为 False：
         if element > 62:
+            filter_arr.append(True)
+        else:
+            filter_arr.append(False)
+
+    newarr = arr[filter_arr]
+
+    print(filter_arr)
+    print(newarr)
+
+    # 找偶数
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+    # 创建一个空列表
+    filter_arr = []
+
+    # 遍历 arr 中的每个元素
+    for element in arr:
+        # 如果元素可以被 2 整除，则将值设置为 True，否则设置为 False
+        if element % 2 == 0:
             filter_arr.append(True)
         else:
             filter_arr.append(False)
