67f1676a33a675b895ddb8e2ec37b9df15a04f35
EndanLu <luendan@yeah.net>
2023-05-11 15:45:34 +0800

    commit from PyCharm IDE.

--- NumPyOperations.py
+++ NumPyOperations.py
@@ -343,9 +343,9 @@ def testNumPyPercentile():
     print(x)
 
 def testNumPyUniformHist():
-    x = np.random.uniform(0.0, 5.0, 250)
+    x = np.random.uniform(0.0, 255.0, 500)    # 500个浮点随机数
     print(x)
 
-    plt.hist(x, 5)
+    plt.hist(x, 50)                           # 直方图
     plt.show()
 
