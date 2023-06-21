ba61f174b96aeb25917ab8065ba3c360ad0d0a36
EndanLu <luendan@yeah.net>
2023-06-21 20:01:10 +0800

    Add MySQLHelper class.

--- TryWhisperTool.py
+++ TryWhisperTool.py
@@ -34,4 +34,55 @@ def test_check_phone():
     print('check_phone检毕通过')
 
 
+# 创建一个类，用于操作MySQL数据库，对表的字段进行增删改查。
+class MySQLHelper:
+    def __init__(self, host, port, user, password, database):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
 
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        import pymysql
+        self.conn = pymysql.connect(host=self.host, port=self.port, user=self.user, password=self.password, database=self.database)
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+    def insert(self, table, **kwargs):
+        keys = list(kwargs.keys())
+        values = list(kwargs.values())
+        sql = 'insert into %s (%s) values (%s)' % (table, ','.join(keys), ','.join(['%s'] * len(values)))
+        self.cursor.execute(sql, values)
+        self.conn.commit()
+
+
+    def delete(self, table, **kwargs):
+        keys = list(kwargs.keys())
+        values = list(kwargs.values())
+        sql = 'delete from %s where %s' % (table, ' and '.join(['%s=%s'] * len(keys)))
+        self.cursor.execute(sql, values)
+        self.conn.commit()
+
+
+    def update(self, table, **kwargs):
+        keys = list(kwargs.keys())
+        values = list(kwargs.values())
+        sql = 'update %s set %s where %s' % (table, ','.join(['%s=%s'] * len(keys)), ','.join(['%s=%s'] * len(keys)))
+        self.cursor.execute(sql, values)
+        self.conn.commit()
+
+# 针对上面的类，写一些测试代码
+def test_MySQLHelper():
+    helper = MySQLHelper('localhost', 3306, 'root', '123456', 'test')
+    helper.connect()
+    helper.insert('user', name='小明', age=18)
+    helper.update('user', name='小明', age=19)
+    helper.delete('user', name='小明')
+    helper.close()
