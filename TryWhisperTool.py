0621dd5c76c856f81224595cad59bddbf5cf670f
EndanLu <luendan@yeah.net>
2023-06-29 10:30:55 +0800

    Add MySQLHelper class.

--- TryWhisperTool.py
+++ TryWhisperTool.py
@@ -7,7 +7,7 @@ def count_letter(str):
         dict[i] = dict.get(i, 0) + 1
     max_key = max(dict, key=dict.get)
     print(max_key, dict[max_key])
-    return max_key, dict[max_key]
+    return (max_key, dict[max_key])
 
 # 为count_letter函数添加单元测试代码
 def test_count_letter():
