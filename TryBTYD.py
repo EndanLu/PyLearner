ec234198945a3e6e48e0a2a8c37696d9f392cdc9
EndanLu <luendan@yeah.net>
2024-07-24 17:05:35 +0800

    Add New File, trying to utilize BTYD library to calculate CLV. (BTYD: Buy Till You Die) (CLV: Customer Lifetime Value)

---
+++ TryBTYD.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Option A:
+# BTYD Library:
+# https://btyd.readthedocs.io/en/latest/index.html
+# User Guide:
+# https://btyd.readthedocs.io/en/latest/User%20Guide.html
+
+# Option B:
+# https://www.kaggle.com/code/mursideyarkin/buy-till-you-die-models-customer-lifetime-value/notebook
+
+
+
